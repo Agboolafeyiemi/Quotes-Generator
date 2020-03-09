2e3a21cd4e03e2f805e21268fc0bcf1cbe5c3658
agboola.omowumi <agboola.omowumi@konga.com>
2020-03-09 13:45:19 +0100

    Add index.js file

--- index.js
+++ index.js
@@ -152,33 +152,27 @@ var colors = [
 ];
 let timer;
 
-// generates a random number, assigns it to a variable, then uses it to return a random object from the quotes array
 function getRandomQuote() {
    let randomQuote = Math.floor(Math.random() * quotes.length);
 
    return quotes[randomQuote];
 }
 
-// generates a random number, assigns it to a variable, then uses it to return a random object from the colors array
 function getRandomColor() {
    let randomColor = Math.floor(Math.random() * colors.length);
 
    return colors[randomColor];
 }
 
-// assigns a setInterval method to the variable so that the printQuote function will automatically run after 10 seconds
 function startTimer() {
    timer = setInterval(printQuote, 10000);
 }
 
-// clears the setInterval method from the timer variable
 function clearTimer() {
    clearInterval(timer);
 }
 
 function printQuote() {
-   // creates the currentQuoute variable and sets the value to the random object that is returned when the getRandomQuote function is called
-   // creates the currentColor variable and sets the value to the random object that is returned when the getRandomColor function is called
    // creates the html variable and uses the currentQuote variable along with key values to build a string
    let currentQuote = getRandomQuote();
    let currentColor = getRandomColor();
@@ -188,15 +182,13 @@ function printQuote() {
    if ("citation" in currentQuote) {
        html += "<span class='citation'> " + currentQuote.citation + "</span>";
    }
-   // tests to see if the date property is present in the currentQuote and if so, adds it to the string
+   // tests date too
    if ("date" in currentQuote) {
        html += "<span class='year'> " + currentQuote.date + "</span>";
    }
    html += "<span class='category'> " + currentQuote.category + "</span>";
 
    // writes the info from the html variable to the div with the quote-box id
-   // uses the currentColor variable to change the background color
-   // uses the currentColor variable to change the button color
    document.getElementById("quote-box").innerHTML = html;
    document.body.style.background = currentColor.background;
    document.getElementById("loadQuote").style.background = currentColor.button;
@@ -210,5 +202,4 @@ function printQuote() {
 printQuote();
 
 // event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
